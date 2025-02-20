5064d5dd171526a90670e5eeed4d474ed38eee40
mluud <maretluud@gmail.com>
2025-02-20 20:03:09 +0200

    Worked on light-dark mode in class

---
+++ Lab3/site1-start/logic.js
@@ -0,0 +1,34 @@
+const themeBtn = document.querySelector("#theme-toggle");
+const modeLight = document.querySelector("#mode-light");
+const modeDark = document.querySelector("#mode-dark");
+
+modeDark.style.display = "none"
+
+const body = document.body;
+if(localStorage.getItem("currentTheme") === "dark")
+{
+    body.classList.add("dark-mode");
+    modeDark.style.display = "none";
+    modeLight.style.display = "inline";
+}
+else
+{
+    modeDark.style.display = "none";
+    modeLight.style.display = "inline";
+}
+
+themeBtn.addEventListener("click", ()=> {
+    body.classList.toggle("dark-mode");
+
+    if(body.classList.contains("dark-mode"))
+    {
+        modeDark.style.display = "none"
+        modeLight.style.display = "inline"
+        localStorage.setItem("currentTheme", "light")
+    }
+    else{
+        modeLight.style.display = "none"
+        modeDark.style.display = "inline"
+        localStorage.setItem("currentTheme", "dark")
+    }
+});
