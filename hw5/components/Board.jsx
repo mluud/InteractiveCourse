f5331219b4a668ff3010b2b49d6ab553111daedf
mluud <maretluud@gmail.com>
2025-03-19 22:57:24 +0200

    Finished tictactoe

---
+++ hw5/components/Board.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import OneButton from '../components/OneButton'
+
+function Board({ xIsNext, squares, onPlay }) {
+    
+    function handleClick(i) {
+      if (squares[i] || calculateWinner(squares)) {
+        return;
+      }
+      
+      const nextSquares = squares.slice();
+  
+      if (xIsNext) {
+        nextSquares[i] = "X";
+      } else {
+        nextSquares[i] = "O";
+      }
+      onPlay(nextSquares);
+    }
+  
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+  
+    return (
+      <div id="buttonGrid">
+        <div className="status">{status}</div>
+        <div className="board-row">
+          <OneButton value={squares[0]} onSquareClick={() => handleClick(0)}/>
+          <OneButton value={squares[1]} onSquareClick={() => handleClick(1)}/>
+          <OneButton value={squares[2]} onSquareClick={() => handleClick(2)}/>
+        </div>
+        <div className="board-row">
+          <OneButton value={squares[3]} onSquareClick={() => handleClick(3)}/>
+          <OneButton value={squares[4]} onSquareClick={() => handleClick(4)}/>
+          <OneButton value={squares[5]} onSquareClick={() => handleClick(5)}/>
+        </div>
+        <div className="board-row">
+          <OneButton value={squares[6]} onSquareClick={() => handleClick(6)}/>
+          <OneButton value={squares[7]} onSquareClick={() => handleClick(7)}/>
+          <OneButton value={squares[8]} onSquareClick={() => handleClick(8)}/>
+        </div>
+      </div>
+    )
+  }
+  
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+export default Board
